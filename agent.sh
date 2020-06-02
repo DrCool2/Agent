d9b45def278026bc4f85e2c231a6081ab5268a2e
Dr.Cool <DrCoolNow@gmail.com>
2020-06-02 15:54:36 -0400

    hoping this will update the Sudoers file

--- agent.sh
+++ agent.sh
@@ -22,16 +22,14 @@ echo "Running yum install for sysadmin tools"
 yum -y install tmux nano sudo
 
 echo "Installing public keys"
-if [[ -d "/root/.ssh" ]]
-then
+if [[ -d "/root/.ssh" ]] then
  sudo rm -r /root/.ssh/
 fi
   mkdir /root/.ssh/
   chmod 700 /root/.ssh/
   cp ./authorized_keys /root/.ssh/
 
-if [[ -d "$UserDir/.ssh"  ]]
-then
+if [[ -d "$UserDir/.ssh"  ]] then
   rm -r $UserDir/.ssh
 fi
   mkdir $UserDir/.ssh
@@ -41,12 +39,21 @@ fi
 
 
 echo "setting up Sudo to run without requiring a password"
-if [[ ! -d ./ORIGINAL  ]]
-then
-echo "./ORIGINAL does NOT exist. Creating it now..."
-mkdir ./ORIGINAL
-cp /etc/sudoers ./ORIGINAL/
-cp ./sudoers /etc/sudoers
+if [[ ! -d ./ORIGINAL  ]] then
+    echo "./ORIGINAL does NOT exist. Creating it now..."
+    mkdir ./ORIGINAL
+    echo "adding USER1 to WHEEL"
+    sudo usermod -aG wheel user1
+ 
+    echo "confirming that USER1 is a member of the WHEEL group."
+    sudo -l
+
+    cp /ect/sudoers ./ORIGINAL/sudoers-date +"%m_%d_%Y"
+    echo "updating SUDOERS file."
+    if [ -z "$(grep 'user1 ALL=(ALL:ALL) ALL' /etc/sudoers )" ]; then echo "user1 ALL=(ALL:ALL) ALL" | sudo EDITOR='tee -a' visudo; fi; # this code was gratefully used from the following site: https://stackoverflow.com/questions/323957/how-do-i-edit-etc-sudoers-from-a-script
+
+#    cp /etc/sudoers ./ORIGINAL/
+#    cp ./sudoers /etc/sudoers
 fi
 
 exit
