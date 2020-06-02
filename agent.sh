985fd06b654837f0d82e1a3c1045dac69a94e52d
Dr.Cool <DrCoolNow@gmail.com>
2020-06-02 14:49:45 -0400

    commented out the update Sudoers file

--- agent.sh
+++ agent.sh
@@ -10,7 +10,8 @@ systemctl stop firewalld
 systemctl disable firewalld
 
 echo "Running yum update"
-yum update
+#yum update
+yum check-update
 
 echo "Running yum install for sysadmin tools"
 #this command will work for interactive sessions but it's likely that " /usr/bin/apt-get -qq --no-upgrade" will need to be used for when we progress to being run by cron
@@ -21,6 +22,13 @@ mkdir /root/.ssh/
 chmod 700 /root/.ssh/
 cp ./authorized_keys /root/.ssh/
 cp ./authorized_keys $UserDir/.ssh/
+
+
+#echo "setting up Sudo to run without requiring a password"
+#mkdir ./ORIGINAL
+#cp /etc/sudoers ./ORIGINAL/
+#cp ./sudoers /etc/sudoers
+
 
 echo "Agent completed running."
 
