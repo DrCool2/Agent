cab3a01b7cba3bf077298e321ce8b79e87f7403a
Dr.Cool <DrCoolNow@gmail.com>
2020-06-02 14:11:56 -0400

    got initial config for agent.sh working

--- agent.sh
+++ agent.sh
@@ -9,14 +9,16 @@ echo "Stopping and disabling firewalld"
 systemctl stop firewalld
 systemctl disable firewalld
 
-echo "Running apt update"
-apt update
+echo "Running yum update"
+yum update
 
-echo "Running apt install for sysadmin tools"
+echo "Running yum install for sysadmin tools"
 #this command will work for interactive sessions but it's likely that " /usr/bin/apt-get -qq --no-upgrade" will need to be used for when we progress to being run by cron
-apt-get -y install tmux nano sudo
+yum -y install tmux nano sudo
 
 echo "Installing public keys"
+mkdir /root/.ssh/
+chmod 700 /root/.ssh/
 cp ./authorized_keys /root/.ssh/
 cp ./authorized_keys $UserDir/.ssh/
 
