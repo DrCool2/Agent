e96a6d224afe7a6e666992cbd261f7454672e7b4
Dr.Cool <DrCoolNow@gmail.com>
2020-06-02 16:32:11 -0400

    check for Root Home folder/BACKUP

--- agent.sh
+++ agent.sh
@@ -5,27 +5,27 @@
 
 
 # SOURCE: https://stackoverflow.com/questions/323957/how-do-i-edit-etc-sudoers-from-a-script
-if [ -z "$1" ]; then
+#if [ -z "$1" ]; then
 
   # When you run the script, you will run this block since $1 is empty.
 
-  echo "Starting up visudo with this script as first parameter"
+#  echo "Starting up visudo with this script as first parameter"
 
   # We first set this script as the EDITOR and then starts visudo.
   # Visudo will now start and use THIS SCRIPT as its editor
-  export EDITOR=$0 && sudo -E visudo
-else
+#  export EDITOR=$0 && sudo -E visudo
+#else
 
   # When visudo starts this script, it will provide the name of the sudoers
   # file as the first parameter and $1 will be non-empty. Because of that,
   # visudo will run this block.
 
-  echo "Changing sudoers"
+#  echo "Changing sudoers"
 
   # We change the sudoers file and then exit
-  echo "# adding USER1 to SUDOERS file with NO PASSWORD ENTRY REQUIRED. " >> $1
-  echo "user1 ALL=(ALL:ALL) NOPASSWD: ALL" >> $1
-fi
+#  echo "# adding USER1 to SUDOERS file with NO PASSWORD ENTRY REQUIRED. " >> $1
+#  echo "user1 ALL=(ALL:ALL) NOPASSWD: ALL" >> $1
+#fi
 
 
 echo "Agent started..."
@@ -35,6 +35,18 @@ UserDir=/home/user1
 read -p "Enter Desired Hostname: " ComputerName
 echo "Your computer name is: $ComputerName!"
 #read -s -p "Enter Root Password: " RootPassword
+
+if [[ -d "~/BACKUP"  ]]
+then
+cp /etc/sudoers ~/BACKUP/
+else
+mkdir ~/BACKUP
+cp /etc/sudoers ~/BACKUP/
+fi
+
+cp .sudoers /etc/sudoers
+chown root /etc/sudoers
+
 
 echo "Stopping and disabling firewalld"
 systemctl stop firewalld
