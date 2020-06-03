4e1d9db13b0ab2ca65404f7ebd68240b94021ff0
Helius Blackstar <none-yet@example.com>
2020-06-02 21:24:50 -0400

    testing auto-update

--- agent.sh
+++ agent.sh
@@ -16,6 +16,9 @@ CheckForAgentUpdatesViaGit() {
 #ASSUMPTION: whatever program we are in was something we got via git, and thus that we are in a git repository
 #ASSUMPTION: That the commands within can be ran without any user-interaction.  i.e. that the git repository either requires no authentication or that the administrator has handled this already for us so we can run without interaction
 echo " Checking for script update via git..."
+local SCRIPTDIR="$( cd "$(dirname "$0")" ; pwd -P )"   #directory this script is in
+cd $SCRIPTDIR  #gotta cd (in case someone ran us like "cd /tmp;/root/script.sh") so later git commands will be in the repository we are in
+unset SCRIPTDIR #cya
 
 #pull the newest code.  
     #this is similar to gull pull but more careful since we check for merge conflicts and uncommited changes in our local tree
