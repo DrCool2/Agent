3dab21033832cc56a99260755ed1cf43ce5d96d1
Dr.Cool <DrCoolNow@gmail.com>
2020-06-03 09:33:07 -0400

    Adding Ruby and Rails Installer

--- agent.sh
+++ agent.sh
@@ -69,6 +69,7 @@ fi
 
 
 echo "Installing Ruby on Rails lastest"
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 ruby -v
 rails -v
