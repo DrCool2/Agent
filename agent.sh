ace8c7b9e4235ea6c3397124c0210944946cd74a
Helius Blackstar <none-yet@example.com>
2020-06-02 21:42:52 -0400

    testing auto-update

--- agent.sh
+++ agent.sh
@@ -15,7 +15,7 @@ GitUpdate() {
 #PURPOSE: this function checks for script updates via git.  It then performs an update via git and launches the new copy of the calling script.  this is vaguely similar to gull pull but more careful since we check for merge conflicts and uncommited changes in our local tree
 #ASSUMPTION: whatever program we are in was something we got via git, and thus that we are in a git repository
 #ASSUMPTION: That the commands within can be ran without any user-interaction.  i.e. that the git repository either requires no authentication or that the administrator has handled this already for us so we can run without interaction
-echo " Checking for script update via git..."
+echo " GitUpdate begin."
 local SCRIPTDIR="$( cd "$(dirname "$0")" ; pwd -P )"   #directory this script is in
 cd $SCRIPTDIR  #gotta cd (in case someone ran us like "cd /tmp;/root/script.sh") so later git commands will be in the repository we are in
 unset SCRIPTDIR #our time together was so short
@@ -46,7 +46,7 @@ unset SCRIPTDIR #our time together was so short
         if [ "$?" = "0" ]; then
         echo " Git says we are already up to date..."
         #no further action needed, we can just run what we have locally since it's up to date
-        echo " Completed checking for script update via git..."
+        echo " GitUpdate End."
         return #run rest of program
         fi
 
@@ -58,6 +58,7 @@ unset SCRIPTDIR #our time together was so short
     if [ "$?" = "0" ]; then
     echo " No git errors detected during our upgrade..."
     echo " Launching updated copy of script..."
+    echo " GitUpdate End."
     $0 #launch updated copy of script
     exit #abort this older copy
     else
